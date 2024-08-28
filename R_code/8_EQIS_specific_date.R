cc639a97e28269fc404ed516b8506d2995781915
Inesmsrl <ines.masurel@gmail.com>
2024-08-28 17:10:41 +0200

    Corrected the code to compare the same population structure and evaluation of the avoided deaths in 2050 in each scenario

--- R_code/8_EQIS_specific_date.R
+++ R_code/8_EQIS_specific_date.R
@@ -26,7 +26,7 @@ combined_rr_sig <- import(here("data_clean", "combined_rr_sig.xlsx"))
 
 # Bornes temporelles du modèle
 year_i <- 2020 # Année initiale
-year_c <- 2035 # Année d'intérêt
+year_c <- 2050 # Année d'intérêt
 
 ################################################################################################################################
 #                                             3. Préparation des données                                                       #
@@ -37,13 +37,10 @@ year_c <- 2035 # Année d'intérêt
     select("age", !!sym(as.character(year_i))) %>% 
     rename("mr_year_i" = !!sym(as.character(year_i)))
 
-# Structure de la population l'année initiale et l'année d'intérêt
+# Structure de la population l'année initiale
   pop <- population %>% 
-    select("age", !!sym(as.character(year_i)), !!sym(as.character(year_c))) %>% 
-    pivot_longer(cols = c(!!sym(as.character(year_i)), !!sym(as.character(year_c))),
-                 names_to = "year",
-                 values_to = "population") %>% 
-    mutate(year = as.numeric(year))
+    select("age", !!sym(as.character(year_i))) %>% 
+    rename("population" = !!sym(as.character(year_i)))
 
 # RR combinés de l'année initiale et l'année d'intérêt pour chaque scénario
   rr <- combined_rr_sig %>% 
@@ -63,10 +60,9 @@ MR_adjusted <- MR_year_i %>%
 #                                             5. EQIS                                                                          #
 ################################################################################################################################
 
-# Nombre de décès les années de base et d'intérêt
+# Nombre de décès les années initiales et d'intérêt
   deaths <- pop %>% 
-    inner_join(MR_adjusted, by = c("age", 
-                                 "year")) %>% 
+    inner_join(MR_adjusted, by = "age") %>% 
     mutate(deaths = population * adjusted_mr) %>% 
     select("age", "year", "scenario", "deaths")
   
@@ -89,4 +85,27 @@ MR_adjusted <- MR_year_i %>%
          subtitle = "sigmoidal implementation of diets",
          x = "Age",
          y = "Avoided deaths")
-    
+  
+  graph_avoided_deaths_2050 <- ggplot(avoided_deaths, aes(x = age,
+                                                          y = avoided_deaths))+
+    facet_wrap(~ scenario, scales = "fixed")+
+    geom_bar(stat = "identity" ,
+             fill = "darkseagreen4")+
+    labs(title = "Avoided deaths in 2050 compared to 2020",
+         subtitle = "sigmoidal implementation of diets",
+         x = "Age",
+         y = "Avoided deaths")
+  
+  
+################################################################################################################################
+#                                             7. Exportation des données                                                       #
+################################################################################################################################
+
+# Décès évités en 2035 par rapport à 2020
+  export(avoided_deaths ,here("results", "avoided_deaths_2035_2020.xlsx"))
+  ggsave(here("results", "avoided_deaths_2035_2020.pdf"), plot = graph_avoided_deaths_2035)
+  
+# Décès évités en 2050 par rapport à 2020
+  export(avoided_deaths ,here("results", "avoided_deaths_2050_2020.xlsx"))
+  ggsave(here("results", "avoided_deaths_2050_2020.pdf"), plot = graph_avoided_deaths_2050)
+  
