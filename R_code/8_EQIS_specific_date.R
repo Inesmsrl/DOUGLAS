f0d9d330df182ba15d8b2a6470f9305632c59126
Inesmsrl <ines.masurel@gmail.com>
2024-08-29 11:44:01 +0200

    Diets applied to 2035 and 2050 population structures

--- R_code/8_EQIS_specific_date.R
+++ R_code/8_EQIS_specific_date.R
@@ -26,7 +26,7 @@ combined_rr_sig <- import(here("data_clean", "combined_rr_sig.xlsx"))
 
 # Bornes temporelles du modèle
 year_i <- 2020 # Année initiale
-year_c <- 2050 # Année d'intérêt
+year_c <- 2035 # Année d'intérêt
 
 ################################################################################################################################
 #                                             3. Préparation des données                                                       #
@@ -37,10 +37,21 @@ year_c <- 2050 # Année d'intérêt
     select("age", !!sym(as.character(year_i))) %>% 
     rename("mr_year_i" = !!sym(as.character(year_i)))
 
+# Taux de mortalité de l'année d'intérêt du modèle
+  MR_year_c <- MR %>%
+    select("age", !!sym(as.character(year_c))) %>% 
+    rename("mr_year_c" = !!sym(as.character(year_c)))
+
 # Structure de la population l'année initiale
-  pop <- population %>% 
+  pop_i <- population %>% 
     select("age", !!sym(as.character(year_i))) %>% 
     rename("population" = !!sym(as.character(year_i)))
+  
+# Structure de la population l'année initiale
+  pop_c <- population %>% 
+    select("age", !!sym(as.character(year_c))) %>% 
+    rename("population" = !!sym(as.character(year_c)))
+  
 
 # RR combinés de l'année initiale et l'année d'intérêt pour chaque scénario
   rr <- combined_rr_sig %>% 
@@ -61,97 +72,190 @@ year_c <- 2050 # Année d'intérêt
 #                                             4. Ajustement des MR                                                             #
 ################################################################################################################################
 
-MR_adjusted <- MR_year_i %>% 
+# Ajustement des MR de l'année initiale du modèle
+  MR_adjusted_i <- MR_year_i %>% 
+      crossing(rr) %>% 
+      mutate(adjusted_mr = mr_year_i * combined_rr / combined_rr[year == year_i]) %>% 
+      select("age", "year", "scenario", "adjusted_mr")
+  
+# Ajustement des MR de l'année d'intérêt du modèle
+  MR_adjusted_c <- MR_year_c %>% 
     crossing(rr) %>% 
-    mutate(adjusted_mr = mr_year_i * combined_rr / combined_rr[year == year_i]) %>% 
+    mutate(adjusted_mr = mr_year_c * combined_rr / combined_rr[year == year_c]) %>% 
     select("age", "year", "scenario", "adjusted_mr")
   
+  
 ################################################################################################################################
-#                                             5. EQIS                                                                          #
+#                                             5. EQIS régimes appliqués à la population initiale                               #
 ################################################################################################################################
 
 # Nombre de décès les années initiales et d'intérêt
-  deaths <- pop %>% 
-    inner_join(MR_adjusted, by = "age") %>% 
+  deaths_i <- pop_i %>% 
+    inner_join(MR_adjusted_i, by = "age") %>% 
     mutate(deaths = population * adjusted_mr) %>% 
     select("age", "year", "scenario", "deaths")
   
 # Nombre de décès évités/ajoutés l'année d'intérêt par rapport à l'année initiale
-  avoided_deaths <- deaths %>%
+  avoided_deaths_i <- deaths_i %>%
     pivot_wider(names_from = "year", values_from = "deaths") %>% 
     mutate(avoided_deaths = get(as.character(year_i)) - get(as.character(year_c))) %>% 
     select("age", "scenario", "avoided_deaths")
   
 # Nombre total de décès évités par scénario
-  total_avoided_deaths <- avoided_deaths %>% 
+  total_avoided_deaths_i <- avoided_deaths_i %>% 
     group_by(scenario) %>% 
     summarize(total_avoided_deaths = sum(avoided_deaths))
 
 ################################################################################################################################
-#                                             6. Représentation graphique                                                      #
+#                                             6. EQIS régimes appliqués à la population d'intérêt                              #
+################################################################################################################################
+  
+# Nombre de décès les années initiales et d'intérêt
+  deaths_c <- pop_c %>% 
+    inner_join(MR_adjusted_c, by = "age") %>% 
+    mutate(deaths = population * adjusted_mr) %>% 
+    select("age", "year", "scenario", "deaths")
+  
+# Nombre de décès évités/ajoutés l'année d'intérêt par rapport à l'année initiale
+  avoided_deaths_c <- deaths_c %>%
+    pivot_wider(names_from = "year", values_from = "deaths") %>% 
+    mutate(avoided_deaths = get(as.character(year_i)) - get(as.character(year_c))) %>% 
+    select("age", "scenario", "avoided_deaths")
+  
+# Nombre total de décès évités par scénario
+  total_avoided_deaths_c <- avoided_deaths_c %>% 
+    group_by(scenario) %>% 
+    summarize(total_avoided_deaths = sum(avoided_deaths))
+  
+################################################################################################################################
+#                                             6. Représentations graphiques                                                    #
 ################################################################################################################################
 
+## Régimes appliqués à la structure de population de l'année initiale du modèle
+
 # Décès évités par âge
-  graph_avoided_deaths_2035 <- ggplot(avoided_deaths, aes(x = age,
-                                                          y = avoided_deaths))+
+  graph_avoided_deaths_2035_i <- ggplot(avoided_deaths_i, aes(x = age,
+                                                              y = avoided_deaths))+
     facet_wrap(~ scenario, scales = "fixed")+
     geom_bar(stat = "identity" ,
              fill = "maroon")+
     labs(title = "Avoided deaths in 2035 compared to 2020",
-         subtitle = "sigmoidal implementation of diets",
+         subtitle = "sigmoidal implementation of diets applied to 2020 population",
          x = "Age",
          y = "Avoided deaths")
   
-  graph_avoided_deaths_2050 <- ggplot(avoided_deaths, aes(x = age,
-                                                          y = avoided_deaths))+
+  graph_avoided_deaths_2050_i <- ggplot(avoided_deaths_i, aes(x = age,
+                                                              y = avoided_deaths))+
     facet_wrap(~ scenario, scales = "fixed")+
     geom_bar(stat = "identity" ,
              fill = "darkseagreen4")+
     labs(title = "Avoided deaths in 2050 compared to 2020",
-         subtitle = "sigmoidal implementation of diets",
+         subtitle = "sigmoidal implementation of diets applied to 2020 population",
          x = "Age",
          y = "Avoided deaths")
   
 # Total des décès évités
-  graph_total_avoided_2035 <- ggplot(data = total_avoided_deaths, aes(x = scenario, 
-                                                                      y = total_avoided_deaths, 
-                                                                      fill = scenario))+
+  graph_total_avoided_2035_i <- ggplot(data = total_avoided_deaths_i, aes(x = scenario, 
+                                                                          y = total_avoided_deaths, 
+                                                                          fill = scenario))+
     geom_bar(stat = "identity", alpha = 0.8)+
     labs(title = "Total avoided deaths in 2035 compared to 2020",
-         subtitle = "sigmoidal implementation of diets",
+         subtitle = "sigmoidal implementation of diets applied to 2020 population",
          x = "",
          y = "number of avoided deaths")+
     scale_fill_manual(values = col_scenario)+
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
-  graph_total_avoided_2050 <- ggplot(data = total_avoided_deaths, aes(x = scenario, 
-                                                                      y = total_avoided_deaths, 
+  graph_total_avoided_2050_i <- ggplot(data = total_avoided_deaths_i, aes(x = scenario, 
+                                                                          y = total_avoided_deaths, 
                                                                           fill = scenario))+
     geom_bar(stat = "identity", alpha = 0.8)+
     labs(title = "Total avoided deaths in 2050 compared to 2020",
-         subtitle = "sigmoidal implementation of diets",
+         subtitle = "sigmoidal implementation of diets applied to 2020 population",
+         x = "",
+         y = "number of avoided deaths")+
+    scale_fill_manual(values = col_scenario)+
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+## Régimes appliqués à la structure de population de l'année d'intérêt du modèle
+  
+# Décès évités par âge
+  graph_avoided_deaths_2035_c <- ggplot(avoided_deaths_c, aes(x = age,
+                                                              y = avoided_deaths))+
+    facet_wrap(~ scenario, scales = "fixed")+
+    geom_bar(stat = "identity" ,
+             fill = "maroon")+
+    labs(title = "Avoided deaths in 2035 compared to 2020",
+         subtitle = "sigmoidal implementation of diets applied to 2035 population",
+         x = "Age",
+         y = "Avoided deaths")
+  
+  graph_avoided_deaths_2050_c <- ggplot(avoided_deaths_c, aes(x = age,
+                                                              y = avoided_deaths))+
+    facet_wrap(~ scenario, scales = "fixed")+
+    geom_bar(stat = "identity" ,
+             fill = "darkseagreen4")+
+    labs(title = "Avoided deaths in 2050 compared to 2020",
+         subtitle = "sigmoidal implementation of diets applied to 2050 population",
+         x = "Age",
+         y = "Avoided deaths")
+
+# Total des décès évités
+  graph_total_avoided_2035_c <- ggplot(data = total_avoided_deaths_c, aes(x = scenario, 
+                                                                          y = total_avoided_deaths, 
+                                                                          fill = scenario))+
+    geom_bar(stat = "identity", alpha = 0.8)+
+    labs(title = "Total avoided deaths in 2035 compared to 2020",
+         subtitle = "sigmoidal implementation of diets applied to 2020 population",
          x = "",
          y = "number of avoided deaths")+
     scale_fill_manual(values = col_scenario)+
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
+  graph_total_avoided_2050_c <- ggplot(data = total_avoided_deaths_c, aes(x = scenario, 
+                                                                          y = total_avoided_deaths, 
+                                                                          fill = scenario))+
+    geom_bar(stat = "identity", alpha = 0.8)+
+    labs(title = "Total avoided deaths in 2050 compared to 2020",
+         subtitle = "sigmoidal implementation of diets applied to 2050 population",
+         x = "",
+         y = "number of avoided deaths")+
+    scale_fill_manual(values = col_scenario)+
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
 ################################################################################################################################
 #                                             7. Exportation des données                                                       #
 ################################################################################################################################
 
+## Régimes appliqués à la structure de population de l'année initiale du modèle
+
 # Décès évités en 2035 par rapport à 2020
-  export(avoided_deaths, here("results", "avoided_deaths_2035_2020.xlsx"))
-  export(total_avoided_deaths, here("results", "total_avoided_2035_2020.xlsx"))
+  export(avoided_deaths_i, here("results", "avoided_deaths_2020_pop_2035_diet.xlsx"))
+  export(total_avoided_deaths_i, here("results", "total_avoided_2020_pop_2035_diet.xlsx"))
   
-  ggsave(here("results", "avoided_deaths_2035_2020.pdf"), plot = graph_avoided_deaths_2035)
-  ggsave(here("results", "total_avoided_2035_2020.pdf"), plot = graph_total_avoided_2035)
+  ggsave(here("results", "avoided_deaths_2020_pop_2035_diet.pdf"), plot = graph_avoided_deaths_2035_i)
+  ggsave(here("results", "total_avoided_2020_pop_2035_diet.pdf"), plot = graph_total_avoided_2035_i)
   
 # Décès évités en 2050 par rapport à 2020
-  export(avoided_deaths ,here("results", "avoided_deaths_2050_2020.xlsx"))
-  export(total_avoided_deaths, here("results", "total_avoided_2050_2020.xlsx"))
+  export(avoided_deaths_i ,here("results", "avoided_deaths_2020_pop_2050_diet.xlsx"))
+  export(total_avoided_deaths_i, here("results", "total_avoided_2020_pop_2050_diet.xlsx"))
   
-  ggsave(here("results", "avoided_deaths_2050_2020.pdf"), plot = graph_avoided_deaths_2050)
-  ggsave(here("results", "total_avoided_2050_2020.pdf"), plot = graph_total_avoided_2050)
+  ggsave(here("results", "avoided_deaths_2020_pop_2050_diet.pdf"), plot = graph_avoided_deaths_2050_i)
+  ggsave(here("results", "total_avoided_2020_pop_2050_diet.pdf"), plot = graph_total_avoided_2050_i)
   
+## Régimes appliqués à la structure de population de l'année d'intérêt du modèle
+  
+# Décès évités en 2035 par rapport à 2020
+  export(avoided_deaths_c, here("results", "avoided_deaths_2035_pop_2020_diet.xlsx"))
+  export(total_avoided_deaths_c, here("results", "total_avoided_2035_pop_2020_diet.xlsx"))
+  
+  ggsave(here("results", "avoided_deaths_2035_pop_2020_diet.pdf"), plot = graph_avoided_deaths_2035_c)
+  ggsave(here("results", "total_avoided_2035_pop_2020_diet.pdf"), plot = graph_total_avoided_2035_c)
+  
+# Décès évités en 2050 par rapport à 2020
+  export(avoided_deaths_c ,here("results", "avoided_deaths_2050_pop_2020_diet.xlsx"))
+  export(total_avoided_deaths_c, here("results", "total_avoided_2050_pop_2020_diet.xlsx"))
+  
+  ggsave(here("results", "avoided_deaths_2050_pop_2020_diet.pdf"), plot = graph_avoided_deaths_2050_c)
+  ggsave(here("results", "total_avoided_2050_pop_2020_diet.pdf"), plot = graph_total_avoided_2050_c)
   
