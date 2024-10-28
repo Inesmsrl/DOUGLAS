2d3e6bdfeb916e844cd82466f3ef50b4b45ab148
Inesmsrl <ines.masurel@gmail.com>
2024-10-28 18:13:59 +0100

    YLL prevented and graph

--- R_code/6_Life_expectancy.R
+++ R_code/6_Life_expectancy.R
@@ -16,6 +16,20 @@ pacman::p_load(
 
 population_evo <- import(here("results", "deaths.csv"))
 
+col_scenario <- c("actuel" = "azure4",
+                  "sc0" = "palevioletred3",
+                  "sc1" = "aquamarine3",
+                  "sc2" = "#DDCC77",
+                  "sc3" = "lightskyblue3",
+                  "sc4" = "#882255",
+                  "sc5" = "royalblue4")
+
+labels_scenario <- c("actuel" = "BAU",
+                     "sc1" = "Scenario 1",
+                     "sc2" = "Scenario 2",
+                     "sc3" = "Scenario 3",
+                     "sc4" = "Scenario 4")
+
 ################################################################################################################################
 #                                             3. Espérance de vie conditionnelle                                               #
 ################################################################################################################################
@@ -47,12 +61,70 @@ population_evo <- population_evo %>%
   group_by(age, year, scenario, simulation_id) %>% 
   mutate(le = age + ex,
          ylg = avoided_deaths * (le - age)) %>% 
-  select(simulation_id, age, year, scenario, avoided_deaths, le)
+  select(simulation_id, age, year, scenario, deaths, avoided_deaths, le, ylg)
 
-  
+yll <- population_evo %>% 
+  group_by(simulation_id, scenario, year) %>% 
+  summarise(yll = sum(ylg))
+
+yll <- yll %>% 
+  group_by(scenario, year) %>% 
+  filter(between(yll, quantile(yll, 0.025), quantile(yll, 0.975)))
+
+summary_yll <- yll %>% 
+  group_by(scenario, year) %>% 
+  summarise(
+    mean_yll = mean(yll, na.rm = TRUE),
+    lower_ci = quantile(yll, 0.025, na.rm = TRUE),
+    upper_ci = quantile(yll, 0.975, na.rm = TRUE)
+  )
+
+graph_yll <- ggplot(summary_yll %>% 
+                                        filter(scenario != "actuel"),
+                                      aes(x = year,
+                                          y = mean_yll,
+                                          group = scenario,
+                                          color = scenario)) +
+  geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci, fill = scenario), alpha = 0.5, linetype = 0)+
+  geom_line(size = 0.6, na.rm = TRUE)+ 
+  labs(
+    title = "",
+    x = "",
+    y = "YLL prevented"
+  )+
+  scale_color_manual(values = col_scenario,
+                     labels = labels_scenario)+
+  scale_fill_manual(values = col_scenario,
+                    labels = labels_scenario)+
+  theme(axis.text.x = element_text(angle = 60, hjust = 1, size = 7),
+        axis.text.y = element_text(size = 7),
+        strip.text = element_text(face = "bold",size = rel(1)),
+        legend.position = "bottom")+
+  guides(color = guide_legend(title = NULL),
+         fill = guide_legend(title = NULL))
+
+# tot_deaths <- population_evo %>% 
+#   group_by(simulation_id, scenario, year) %>% 
+#   summarise(tot_deaths = sum(deaths))
+#   
+#   le <- le %>% 
+#     group_by(simulation_id, scenario, year) %>% 
+#     mutate(yll = age * deaths) %>% 
+#     summarise(yll_sum = sum(yll))
+#   
+#   le <- le %>% 
+#     left_join(tot_deaths, by = c("simulation_id", "scenario", "year"))
+#   
+#   le <- le %>% 
+#     group_by(simulation_id, year, scenario) %>% 
+#     mutate(mean_age_deaths = yll_sum / tot_deaths)
+            
+
 
 ################################################################################################################################
 #                                             4. Exportation des données                                                       #
 ################################################################################################################################
 
-export(le, here("data_clean", "life_expectancy.xlsx"))
+export(summary_yll, here("results", "IC95_yll.xlsx"))
+ggsave(here("results", "yll_reported.pdf"))
+
