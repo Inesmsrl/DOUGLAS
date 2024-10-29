b5742b6cbd8b1cc659fcb2280ead9dc8e1980b16
Inesmsrl <ines.masurel@gmail.com>
2024-10-29 15:35:30 +0100

    R code to plot the sensitivity analysis on the number of deaths prevented

---
+++ R_code/18_Sensitivity_Analysis.R
@@ -0,0 +1,88 @@
+################################################################################################################################
+#                                             1. Chargement des packages                                                       #
+################################################################################################################################
+
+pacman::p_load(
+  rio,                 # Importation de fichiers
+  here,                # Localisation des fichiers dans le dossier du projet
+  dplyr,               # Manipulation des données
+  tidyr,
+  tidyverse
+)
+
+################################################################################################################################
+#                                             2. Importation des données                                                       #
+################################################################################################################################
+
+sensi_data <- import(here("data", "sensitivity_analysis.xlsx"))
+
+col_scenario <- c("actuel" = "azure4",
+                  "sc0" = "palevioletred3",
+                  "sc1" = "aquamarine3",
+                  "sc2" = "#DDCC77",
+                  "sc3" = "lightskyblue3",
+                  "sc4" = "#882255",
+                  "sc5" = "royalblue4")
+
+labels_scenario <- c("actuel" = "BAU",
+                     "sc1" = "Scenario 1",
+                     "sc2" = "Scenario 2",
+                     "sc3" = "Scenario 3",
+                     "sc4" = "Scenario 4")
+
+labels_analysis <- c("main" = "Main Analysis",
+                     "sensi1" = "Immediate time to full effect",
+                     "sensi2" = "Linear 20-year time to full effect",
+                     "sensi3" = "Sigmoidal 10-year time to full effect",
+                     "sensi4" = "No RR effect reduction",
+                     "sensi5" = "50% RR effect reduction",
+                     "sensi6" = "Immediate diet implementation",
+                     "sensi7" = "Linear diet implementation",
+                     "sensi8" = "Sigmoidal diet implementation")
+
+order_scenarios <- c("sc1", "sc2", "sc3", "sc4")
+
+sensi_data$scenario <- factor(sensi_data$scenario, levels = order_scenarios)
+
+################################################################################################################################
+#                                             3. Représentation graphique                                                      #
+################################################################################################################################
+
+graph_sensi <- ggplot(sensi_data %>% 
+         filter(scenario != "actuel"), 
+       aes(x = avoided_deaths,
+                       y = scenario,
+                       color = factor(scenario)))+
+  geom_point(position = position_dodge(width = 0.5), size = 0.7)+
+  geom_segment(aes(x = ic_lower,
+               xend = ic_upper,
+               y = scenario,
+               yend = scenario),
+               position = position_dodge(width = 0.5),
+               linewidth = 0.3)+
+  facet_grid(rows = vars(analysis),
+             cols = vars(year),
+             labeller = labeller(analysis = labels_analysis)
+             )+
+  scale_color_manual(values = col_scenario,
+                     labels = labels_scenario)+
+  labs(title = "",
+       y = NULL,
+       x = "Number of deaths prevented",
+       color = "Scenario")+
+  theme(strip.text.y = element_text(angle = 0, hjust = 0, size = 9),
+        axis.text.x = element_text(angle = 0, hjust = 1, size = 7),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "bottom")+
+  scale_x_continuous(limits = c(0, max(sensi_data$ic_upper, na.rm = TRUE)),
+                     breaks = c(100000),
+                     labels = scales::label_comma())+
+  geom_vline(xintercept = 100000, linetype = "dashed", color = "black", size = 0.3)+
+  guides(color = guide_legend(title = NULL))
+
+################################################################################################################################
+#                                             4. Exportation des données                                                      #
+################################################################################################################################
+
+ggsave(here("results", "sensitivity_analysis.pdf"), graph_sensi)
