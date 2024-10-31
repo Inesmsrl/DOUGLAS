abfd51c08f0a6e0375a2cf778c5fc42badc34b34
Inesmsrl <ines.masurel@gmail.com>
2024-10-31 13:25:15 +0100

    Minor changes

--- R_code/16_RR_distributions.R
+++ R_code/16_RR_distributions.R
@@ -45,7 +45,7 @@ col_food_groups <- c("red_meat" = "#F60239",
   interpolation <- "spline"
   
 # Nombre de simulations des valeurs de RR
-  n <- 1000
+  n <- 200
   
 ################################################################################################################################
 #                                             3. Préparation des données                                                       #
