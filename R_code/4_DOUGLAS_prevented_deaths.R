58dad4399c4991ff6d3ce45ef4dead2d3dca2a9f
Inesmsrl <ines.masurel@gmail.com>
2025-02-12 11:40:11 +0100

    Table summarizing the variations in intakes compared to the baseline scenario and their contributions to the results

--- R_code/4_DOUGLAS_prevented_deaths.R
+++ R_code/4_DOUGLAS_prevented_deaths.R
@@ -9,7 +9,8 @@ pacman::p_load(
   tidyr,               # Manipulation des données
   tidyverse,           # Data management, inclus ggplot
   purrr,               # Opérations itératives
-  psych                # Contient fonction moyenne géométrique
+  flextable,           # Création de tableaux
+  scales               # Transformations en pourcentages notamment
 )
 
 ################################################################################################################################
@@ -1175,12 +1176,61 @@ graph_avoided_deaths_dates <- ggplot(simulations_summary_avoided_deaths %>%
     inner_join(diets_var, by = c("scenario", "year", "food_group")) %>% 
     group_by("scenario", "year", "food_group") %>%
     mutate(deaths = delta * mean_total_deaths /100,
-           quantity = round(quantity),
-           var = round(var, digits = 2),
-           delta = round(delta, digits = 2),
-           deaths = round(deaths)) %>% 
+           var = percent(var/100, accuracy = 0.1),
+           delta = percent(delta/100, accuracy = 0.1)) %>% 
     ungroup() %>%
-    select(scenario, year, food_group, quantity, var, delta, deaths)
+    select(scenario, year, food_group, quantity, var, delta, deaths) %>% 
+    mutate(quantity = round(quantity),
+           deaths = round(deaths))
+  
+  
+  # Contributions pour l'année 2050
+  
+  contrib_2050 <- summary_contrib %>% 
+    filter(year == 2050,
+           scenario != "actuel") %>%
+    pivot_wider(names_from = "scenario",
+                values_from = c("quantity", "var", "delta", "deaths")) %>% 
+    select(-"year") %>%
+    select("food_group",
+           "quantity_sc1", "var_sc1", "delta_sc1", "deaths_sc1",
+           "quantity_sc2", "var_sc2", "delta_sc2", "deaths_sc2",
+           "quantity_sc3", "var_sc3", "delta_sc3", "deaths_sc3",
+           "quantity_sc4", "var_sc4", "delta_sc4", "deaths_sc4") %>%
+    mutate(food_group = labels_food_groups[food_group]) %>% # Remplacer les noms des groupes alimentaires par les labels)
+    qflextable() %>% # Création du tableau et ajustement automatique de la largeur des colonnes
+    add_header_row(top = TRUE, # Ajout d'une ligne d'en-tête
+                   values = c("Food group", "S1", "", "", "", "S2", "", "", "", "S3", "", "", "", "S4", "", "", "")) %>% 
+    set_header_labels( # Renommer des colonnes de la 2e ligne d'en-tête
+      "food_group" = "",
+      "quantity_sc1" = "Intake (g/d/pers)",
+      "var_sc1" = "Intake variation vs baseline",
+      "delta_sc1" = "Contribution to results",
+      "deaths_sc1" = "Deaths due to intake variation",
+      "quantity_sc2" = "Intake (g/d/pers)",
+      "var_sc2" = "Intake variation vs baseline",
+      "delta_sc2" = "Contribution to results",
+      "deaths_sc2" = "Deaths due to intake variation",
+      "quantity_sc3" = "Intake (g/d/pers)",
+      "var_sc3" = "Intake variation vs baseline",
+      "delta_sc3" = "Contribution to results",
+      "deaths_sc3" = "Deaths due to intake variation",
+      "quantity_sc4" = "Intake (g/d/pers)",
+      "var_sc4" = "Intake variation vs baseline",
+      "delta_sc4" = "Contribution to results",
+      "deaths_sc4" = "Deaths due to intake variation"
+    ) %>% 
+    vline(part = "all", j = 5) %>% # Ligne verticale après la colonne 5
+    vline(part = "all", j = 9) %>% # Ligne verticale après la colonne 9
+    vline(part = "all", j = 13) %>% # Ligne verticale après la colonne 13
+    merge_at(i = 1, j = 2:5, part = "header") %>% # Fusion des cellules de la 1ère ligne d'en-tête
+    merge_at(i = 1, j = 6:9, part = "header") %>%
+    merge_at(i = 1, j = 10:13, part = "header") %>%
+    merge_at(i = 1, j = 14:17, part = "header") %>%
+    align(align = "center", j = c(2:17), part = "all") %>%  # Centrer le contenu des cellules sauf Food group
+    bold(i = 1, part = "header") %>% # Mettre en gras la 1ère ligne d'en-tête
+    bg(., i = ~ food_group %in% c("Legumes", "Nuts", "Processed meat", "Red meat", "Eggs", "Vegetables", "Whole grains"), part = "body", bg = "aquamarine2") %>% 
+    bg(., i = ~ food_group %in% c("Fruits", "Fish", "White meat"), part = "body", bg = "indianred1")
   
 ################################################################################################################################
 #                                             24. Exportation des données                                                      #
@@ -1260,3 +1310,5 @@ graph_avoided_deaths_dates <- ggplot(simulations_summary_avoided_deaths %>%
   export(contrib, here("results", "visualization_tool_ic95_sim", "contributions.xlsx"))
   
   ggsave(here("results", "visualization_tool_ic95_sim", "FG_contributions.pdf"), plot = graph_contrib_fg)
+  
+  save_as_image(contrib_2050, here("results", "contributions_2050.png"))
